f45d901b5367e0cdafaa1bbe5f6e04b8a0367731
Dzo00 <58570603+Dzo00@users.noreply.github.com>
2020-09-10 16:16:28 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,21 @@
-window.onload = function(){    
+window.onload = function(){
   $(document).ready(function(){
     $('.carousel').carousel();
   });
+  //RESPONSIVE CSS
+  $(window).resize(PromeniCSSPoVeliciniEkrana);
+  PromeniCSSPoVeliciniEkrana();
+}
+
+//RESPONSIVE CSS FUNKCIJA
+function PromeniCSSPoVeliciniEkrana(){
+  console.log(screen.width);
+  if(screen.width<776)
+  {
+    $('#a-img').removeClass("col");
+  }
+  else
+  {
+    $('#a-img').addClass("col");
+  }
 }
