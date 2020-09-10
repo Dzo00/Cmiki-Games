cfc4ffd13725d270f7e3e6795ff3943fee870788
Dzo00 <58570603+Dzo00@users.noreply.github.com>
2020-09-10 17:30:17 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,33 +24,10 @@ function PromeniCSSPoVeliciniEkrana(){
   }
 }
 
-//REGULRANI IZRAZI
-var reEmail,rePassword;
-reEmail = /^([a-z]{3,}\d{0,}\@((gmail|yahoo)\.com)|ict\.edu\.rs)|([a-z]{3,}\d{0,}\.[a-z]{3,}\d{0,}\@((gmail|yahoo)\.com)|ict\.edu\.rs)$/;
-rePassword= /^(?:(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15})$/;
-
 //LOGOVANJE
 function proveraLog(e){
     e.preventDefault();
-    
-    var email, pass;
-    email = document.getElementById("emailLog").value;
-    pass= document.getElementById("pass").value;
-    let success=false;
-    if(!rePassword.test(pass) || pass==""){
-        $("#pass").css("border-color","#ee0f0f");
-    }
-    else{
-        $("#pass").css("border-color","#08b108");
-        
-    }
-    if(!reEmail.test(email)){
-        $("#emailLog").css("border-color","#ee0f0f").attr("placeholder","eg. john@gmail.com");
-    }
-    else{
-        $("#emailLog").css("border-color","#08b108");
-        success=true;
-    }
+    var success=true;
     if(success){
         window.location.href = "https://dzo00.github.io/Cmiki-Games/index.html";
     }
@@ -59,50 +36,7 @@ function proveraLog(e){
 //REGISTRACIJA
 function proverRegister(e){
     e.preventDefault();
-    
-    var ime, reIme, email, password;
-    ime= document.getElementById("name").value;
-    
-    email = document.getElementById("emailReg").value;
-    password= document.getElementById("password").value;
-    passCheck= document.getElementById("passwordRepeat").value;
-    console.log(password); console.log(passCheck);
-    reIme=/^[A-Z][a-z]{2,12}(\s[A-z][a-z]{2,12})+$/;
-
     let success1=true;
-
-    if(!reIme.test(ime)){
-        $("#name").css("border-color","#ee0f0f").attr("placeholder","eg. John Doe");
-        success1= false;
-    }
-    else{
-        $("#name").css("border-color","#08b108");
-    }
-    if(!rePassword.test(password) || password==""){
-        $("#password").css("border-color","#ee0f0f");
-        success1= false;
-    }
-    else{
-        $("#password").css("border-color","#08b108").next().text(" ");
-    }
-    if(!reEmail.test(email)){
-        $("#emailReg").css("border-color","#ee0f0f").attr("placeholder","eg. john@gmail.com");
-        success1= false;
-    }
-    else{
-        $("#emailReg").css("border-color","#08b108");
-    }
-    if(!rePassword.test(passCheck) && passCheck==""){
-        $("#passwordRepeat").css("border-color","#ee0f0f");
-        success1= false;
-    }
-    else if(passCheck!=password){
-        $("#passwordRepeat").css("border-color","#ee0f0f");
-        success1= false;
-    }
-    else{
-        $("#passwordRepeat").css("border-color","#08b108");
-    }
     if(success1){
         window.location.href = "https://dzo00.github.io/Cmiki-Games/index.html";
     }  
